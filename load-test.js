f61fe40febe40e2c59a5f583f556f73d32567c99
Ruben <24549041+RDieleman@users.noreply.github.com>
2022-12-22 05:31:11 +0100

    Create load-test.js

---
+++ load-test.js
@@ -0,0 +1,23 @@
+import http from 'k6/http';
+import {check, sleep} from 'k6';
+
+export const options = {
+    vus: 50,
+    iterations: 2000,
+    thresholds: {
+        // http errors should be less than 1%
+        http_req_failed: ['rate<0.01'],
+        // 95 percent of response times must be below 500ms
+        http_req_duration: ['p(95)<500']
+    }
+};
+
+export default function () {
+    const url = 'http://www.greatreads.online/';
+
+    const res = http.get(url, {tags: {name: 'Home'}});
+    check(res, {
+        'is status 200': (r) => r.status === 200
+    });
+    sleep(Math.random() * 5);
+}
