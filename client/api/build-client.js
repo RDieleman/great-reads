a88435bcabef2124c22ecb1c732327c3e5dd329b
ruben <24549041+RDieleman@users.noreply.github.com>
2022-12-28 08:12:38 +0100

    Update hostname

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({req}) => {
 
     if (onServer) {
         return axios.create({
-            // baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
+            //dev baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
             baseURL: 'http://www.greatreads.online',
             headers: req.headers,
         });
