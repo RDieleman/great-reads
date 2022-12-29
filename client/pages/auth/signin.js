b30c197e82e29852353c94301b69b24fb3c26f69
ruben <24549041+RDieleman@users.noreply.github.com>
2022-12-29 04:49:58 +0100

    add dirty client implementation for google authentication

--- client/pages/auth/signin.js
+++ client/pages/auth/signin.js
@@ -1,6 +1,7 @@
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import useRequest from "../../hooks/use-request";
 import Router from "next/router";
+import Script from "next/script";
 
 export default () => {
     const [email, setEmail] = useState('');
@@ -13,25 +14,76 @@ export default () => {
         },
         onSuccess: () => Router.push('/')
     });
+    const [idToken, setIdToken] = useState('');
+    const [doAuthGoogle, authGoogleError] = useRequest({
+        url: '/api/users/signin/google',
+        method: 'post',
+        body: {
+            idToken
+        },
+        onSuccess: () => Router.push('/')
+    })
+
+    useEffect(() => {
+        if (idToken) {
+            doAuthGoogle();
+        }
+    }, [idToken])
 
     const onSubmit = async (event) => {
         event.preventDefault();
         await doRequest();
     }
 
-    return <form onSubmit={onSubmit} className="container">
-        <h1>Sign In</h1>
-        <div className="mb-3">
-            <label htmlFor="emailInput" className="form-label">Email address</label>
-            <input type="email" className="form-control" id="emailInput" value={email}
-                   onChange={e => setEmail(e.target.value)}/>
-        </div>
-        <div className="mb-3">
-            <label htmlFor="passwordInput" className="form-label">Password</label>
-            <input type="password" className="form-control" id="passwordInput" value={password}
-                   onChange={e => setPassword(e.target.value)}/>
-        </div>
-        {error}
-        <button className="btn btn-primary">Continue</button>
-    </form>
+    const handleTokenResponse = async (response) => {
+        console.log(response.credential);
+        await setIdToken(response.credential);
+    }
+
+    function loadButton() {
+        google.accounts.id.initialize({
+            client_id: "638484248515-l7015v4n2d37u7titifn64vrolv7bvqu.apps.googleusercontent.com",
+            callback: handleTokenResponse
+        });
+        google.accounts.id.renderButton(
+            document.getElementById("buttonDiv"),
+            {theme: "outline", size: "large"}  // customization attributes
+        );
+        google.accounts.id.prompt();
+    }
+
+    useEffect(() => {
+        try {
+            loadButton()
+        } catch (err) {
+            console.log("Google not yet defined.");
+        }
+    }, []);
+
+    return <div>
+        <Script
+            src="https://accounts.google.com/gsi/client"
+            onLoad={loadButton}
+            async
+            defer
+        />
+        <form onSubmit={onSubmit} className="container">
+            <h1>Sign In</h1>
+            <div className="mb-3">
+                <label htmlFor="emailInput" className="form-label">Email address</label>
+                <input type="email" className="form-control" id="emailInput" value={email}
+                       onChange={e => setEmail(e.target.value)}/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="passwordInput" className="form-label">Password</label>
+                <input type="password" className="form-control" id="passwordInput" value={password}
+                       onChange={e => setPassword(e.target.value)}/>
+            </div>
+            {error}
+            <button className="btn btn-primary">Continue</button>
+        </form>
+        <hr></hr>
+        <div id="buttonDiv"></div>
+    </div>
+
 }
