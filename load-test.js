c75f64d204692c4e5630ad3615e27656c76aff56
ruben <24549041+RDieleman@users.noreply.github.com>
2022-12-30 10:04:18 +0100

    tone down load test

--- load-test.js
+++ load-test.js
@@ -2,8 +2,8 @@ import http from 'k6/http';
 import {check, sleep} from 'k6';
 
 export const options = {
-    vus: 50,
-    iterations: 2000,
+    vus: 10,
+    iterations: 200,
     thresholds: {
         // http errors should be less than 1%
         http_req_failed: ['rate<0.01'],
